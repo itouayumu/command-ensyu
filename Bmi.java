632430475cb15fe9d2116c46bfa5621ac8f8d23c
itou <a.itou.sys22@morijyobi.ac.jp>
2022-11-08 10:47:44 +0900

    説明文追記

--- Bmi.java
+++ Bmi.java
@@ -4,11 +4,18 @@ public class Main{
             
         
       Scanner scanner= new Scanner(System.in);
+      System.out.print("一人目の伸長");
        double height1 = scanner.nextDouble();
+         System.out.print("一人目の体重");
+
        double weight1 = scanner.nextDouble();
+         System.out.print("二人目の伸長");
+
        double height2 = scanner.nextDouble();
+         System.out.print("二人目の体重");
+
        double weight2 = scanner.nextDouble();
-       System.out.print("一人目は"+Math.floor((weight1/(height1*height1)*100))/100);
-       System.out.print("二人目は"+Math.floor((weight2/(height2*height2)*100))/100);
+       System.out.println("一人目は"+Math.floor((weight1/(height1*height1)*100))/100);
+       System.out.println("二人目は"+Math.floor((weight2/(height2*height2)*100))/100);
     }
 }
