d711392a6597657396d69764158a5b23e6ecfa45
itou <a.itou.sys22@morijyobi.ac.jp>
2022-11-08 10:17:09 +0900

    ñl¯ɍs¦é悤ɎdlύX

--- Main.java
+++ Main.java
@@ -4,9 +4,11 @@ public class Main{
             
         
       Scanner scanner= new Scanner(System.in);
-       double height = scanner.nextDouble();
-       double weight = scanner.nextDouble();
-       System.out.print(Math.floor((weight/(height*height)*100))/100);
-
+       double height1 = scanner.nextDouble();
+       double weight1 = scanner.nextDouble();
+       double height2 = scanner.nextDouble();
+       double weight2 = scanner.nextDouble();
+       System.out.print("ˆêl–Ú‚Í"+Math.floor((weight1/(height1*height1)*100))/100);
+       System.out.print("“ñl–Ú‚Í"+Math.floor((weight2/(height2*height2)*100))/100);
     }
 }
